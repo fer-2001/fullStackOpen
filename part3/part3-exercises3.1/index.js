fe10f36cf37b37a0b2a51fe2ce1a2a5bc7a610f5
fer-2001 <mfernandez@dc.exa.unrc.edu.ar>
2024-04-08 12:38:37 -0300

    Connect App to MongoDB

--- part3/part3-exercises3.1/index.js
+++ part3/part3-exercises3.1/index.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 app.use(express.json());
 const cors = require('cors');
 app.use(cors());
 app.use(express.static('dist'))
+const Person = require('./models/person')
 
 let persons = [
   { 
@@ -33,18 +35,64 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 });
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const persona = persons.find(person => person.id === id);
-    if (persona) {
-      response.json(persona);
-    } else {
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
+    if(person){
+      response.json(person)
+    }
+    else{
       response.status(404).end();
     }
+  })
+  .catch(error => next(error))
 });
+
+app.delete('/api/persons/:id', (request, response) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      if (result) {
+        response.status(204).end();
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
+});
+
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// este debe ser el último middleware cargado
+app.use(errorHandler)
+
 
 app.get('/info', (request, response) => {
     let mensaje = "Phone book has info for " + persons.length + " people";
@@ -52,18 +100,6 @@ app.get('/info', (request, response) => {
     response.send(`<p>${mensaje}</p>` + `<p> ${currentTime} </p>`);
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter(person => person.id !== id);
-  response.status(204).end();
-});
-
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0;
-  return maxId + 1;
-};
 
 app.post('/api/persons', (request, response) => {
   const body = request.body;
@@ -74,18 +110,17 @@ app.post('/api/persons', (request, response) => {
     });
   }
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  })
 
-  persons.push(person); // Agregando al final del arreglo
-
-  response.json(person);
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
