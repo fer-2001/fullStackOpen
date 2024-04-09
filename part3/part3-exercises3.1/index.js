0546460444a365caa3ed18759083a338e7201fbf
fer-2001 <mfernandez@dc.exa.unrc.edu.ar>
2024-04-09 17:08:52 -0300

    clean index.js

--- part3/part3-exercises3.1/index.js
+++ part3/part3-exercises3.1/index.js
@@ -1,25 +1,3 @@
-/*require('dotenv').config()
-const express = require('express');
-//const app = express();
-const app = require('./app') // la aplicación Express real
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-app.use(express.json());
-const cors = require('cors');
-app.use(cors());
-app.use(express.static('dist'))
-//const Person = require('./models/person')
-
-
-//const PORT = process.env.PORT
-//app.listen(PORT, () => {
-//  console.log(`Server running on port ${PORT}`)
-//})
-
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
-})*/
-
 const app = require('./app') // la aplicación Express real
 const config = require('./utils/config')
 const logger = require('./utils/logger')
