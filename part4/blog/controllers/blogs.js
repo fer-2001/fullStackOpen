70d6001f0051cf5dab5f539defbbc7a0091a7eb4
fer-2001 <mfernandez@dc.exa.unrc.edu.ar>
2024-04-10 18:58:43 -0300

    Part 4

---
+++ part4/blog/controllers/blogs.js
@@ -0,0 +1,40 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blogs')
+
+
+blogsRouter.get('/', (request, response) => {
+    Blog
+      .find({})
+      .then(blogs => {
+        response.json(blogs)
+      })
+  })
+  
+blogsRouter.post('/', (request, response) => {
+    const blog = new Blog(request.body)
+  
+    blog
+      .save()
+      .then(result => {
+        response.status(201).json(result)
+      })
+  })
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
+    }
+  
+    next(error)
+  }
+  
+// este debe ser el último middleware cargado
+blogsRouter.use(errorHandler)
+
+
+module.exports = blogsRouter
